0f68885b2b22059d056b628637b9fdea6509b791
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2025-01-14 23:01:08 +0700

    fix: index.ts

--- dist/index.js
+++ dist/index.js
@@ -11,7 +11,11 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 const cors = require('cors');
 const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:8080"],
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:8080",
+        "https://music-admin-jet.vercel.app"
+    ],
     optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
--- index.ts
+++ index.ts
@@ -9,7 +9,11 @@ app.use(cookieParser());
 
 const cors = require('cors');
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:8080"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:8080",
+    "https://music-admin-jet.vercel.app"
+  ],
   optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
