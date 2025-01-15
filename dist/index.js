3d2c42cdfb7aba0007c5ec251b4297b1cb158917
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2025-01-15 15:31:27 +0700

    fix: add domain frontend

--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,7 @@ const cors = require('cors');
 const corsOptions = {
     origin: [
         "http://localhost:5173",
+        "https://music-frontend-sigma.vercel.app",
         "http://localhost:8080",
         "https://music-admin-jet.vercel.app"
     ],
--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ const cors = require('cors');
 const corsOptions = {
   origin: [
     "http://localhost:5173",
+    "https://music-frontend-sigma.vercel.app",
     "http://localhost:8080",
     "https://music-admin-jet.vercel.app"
   ],
